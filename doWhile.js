f824f563451d2040f94e1fd61c960ca108f57899
constance khalumba <constancekhalumba@gmail.com>
2024-07-15 14:21:11 +0300

    First commit

---
+++ doWhile.js
@@ -0,0 +1,8 @@
+let numeric = 0;
+
+do{
+    if (numeric % 2 === 0) {
+        console.log("These are even numbers using Do while loops:"+ numeric);
+    }
+    numeric++; // Increment the number
+} while (numeric <= 20); 
---
+++ for.js
@@ -0,0 +1,7 @@
+for (let i = 0; i < 20; i++) {
+    if (i % 2 !== 0){
+        continue;
+    }
+
+    console.log("Loop with continue: even Number"+ i);
+    }  
